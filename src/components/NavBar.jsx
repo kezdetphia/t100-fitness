cc8781719d2375405730360e4af2874f03362f72
kezdetphia <fehermark88@gmail.com>
2023-08-23 10:41:49 +0200

    navbar mobile friendly, spaced out and aligned properly

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,159 +1,52 @@
+import React, { useState } from "react";
+import { IoMenu } from "react-icons/io5";
 
-// import logo from "../assets/img/logo.png";
-// import navIcon1 from "../assets/img/nav-icon1.svg";
-// import navIcon2 from "../assets/img/nav-icon2.svg";
-// import navIcon3 from "../assets/img/nav-icon3.svg";
-// import github from "../assets/img/github.svg";
-import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import { FaBars } from 'react-icons/fa';
-
-
-
-const NavBar = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
-
+const Nav = () => {
+  let Links = [
+    { name: "HOME", link: "/" },
+    { name: "SERVICE", link: "/" },
+    { name: "ABOUT", link: "/" },
+    { name: "BLOG'S", link: "/" },
+    { name: "CONTACT", link: "/" },
+  ];
+  let [open, setOpen] = useState(false);
   return (
-    <nav className="bg-purple-500 p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="text-white font-bold text-xl">Majer Fitness</div>
-        {/* Hamburger icon */}
-        <div className="md:hidden">
-          <button
-            className="text-white focus:outline-none"
-            onClick={toggleMenu}
-          >
-            <FaBars size={24} />
-          </button>
-        </div>
-        {/* Navigation dropdown */}
-        <ul
-          className={`md:flex space-x-4 md:block ${
-            menuOpen ? 'block' : 'hidden'
-          } absolute md:static top-16 right-0 md:top-0 md:right-0 bg-purple-500 md:bg-transparent md:p-0`}
+    <div className="shadow-md w-full fixed top-0 left-0">
+      <div className="md:flex items-center justify-between bg-white py-4 md:px-10 px-7">
+        <div
+          className="font-bold text-2xl cursor-pointer flex items-center font-[Poppins] 
+      text-gray-800"
         >
-          <li>
-            <a href="#" className="text-white hover:underline block md:inline">
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="#" className="text-white hover:underline block md:inline">
-              About
-            </a>
-          </li>
-          <li>
-            <a href="#" className="text-white hover:underline block md:inline">
-              Services
-            </a>
-          </li>
-          <li>
-            <a href="#" className="text-white hover:underline block md:inline">
-              Contact
-            </a>
-          </li>
+          <span className="text-3xl text-indigo-600 mr-1 pt-2"></span>
+          Majer Fitness
+        </div>
+
+        <div
+          onClick={() => setOpen(!open)}
+          className="text-3xl absolute right-8 top-4 cursor-pointer md:hidden"
+        >
+          <IoMenu name={open ? "close" : "menu"}></IoMenu>
+        </div>
+
+        <ul
+        className={`md:flex md:items-center md:pb-0 pb-0 absolute md:static bg-white md:z-auto z-[-1] left-0 right-0 md:w-auto md:pl-0 pl-0 transition-all duration-500 ease-in ${
+          open ? "top-1" : "top-[-490px]"
+        }`}
+      >
+          {Links.map((link) => (
+            <li key={link.name} className="pl-8 md:ml- text-xl md:my-0 my-7">
+              <a
+                href={link.link}
+                className="text-gray-800 hover:text-gray-400 duration-500"
+              >
+                {link.name}
+              </a>
+            </li>
+          ))}
         </ul>
       </div>
-    </nav>
+    </div>
   );
 };
 
-
-export default NavBar;
-
-
-
-
-   
-
-
-// export const NavBar = () => {
-//   const [activeLink, setActiveLink] = useState("home");
-//   const [scrolled, setScrolled] = useState(false);
-
-//   useEffect(() => {
-//     const onScroll = () => {
-//       if (window.scrollY > 50) {
-//         setScrolled(true);
-//       } else {
-//         setScrolled(false);
-//       }
-//     };
-
-//     window.addEventListener("scroll", onScroll);
-
-//     return () => window.removeEventListener("scroll", onScroll);
-//   }, []);
-
-//   const onUpdateActiveLink = (value) => {
-//     setActiveLink(value);
-//   };
-
-
-
-//     <nav className={`navbar ${scrolled ? "scrolled" : ""}`}>
-//       <div className="container mx-auto">
-//         <div className="flex items-center">
-//           <Link to="#home" className="navbar-brand">
-//             {/* <img src={null} alt="Logo" className="h-16 w-16" /> */}
-//             <p>home</p>
-//           </Link>
-//           <button
-//             className="navbar-toggler md:hidden ml-auto"
-//             aria-controls="basic-navbar-nav"
-//           >
-//             <span className="navbar-toggler-icon"></span>
-//           </button>
-//         </div>
-//         <div className="hidden md:flex md:space-x-6 ml-auto">
-//           <Link
-//             to="#home"
-//             className={`navbar-link ${
-//               activeLink === "home" ? "active" : ""
-//             }`}
-//             onClick={() => onUpdateActiveLink("home")}
-//           >
-//             Home
-//           </Link>
-//           <Link
-//             to="#skills"
-//             className={`navbar-link ${
-//               activeLink === "skills" ? "active" : ""
-//             }`}
-//             onClick={() => onUpdateActiveLink("skills")}
-//           >
-//             Skills
-//           </Link>
-//           <Link
-//             to="#projects"
-//             className={`navbar-link ${
-//               activeLink === "projects" ? "active" : ""
-//             }`}
-//             onClick={() => onUpdateActiveLink("projects")}
-//           >
-//             Projects
-//           </Link>
-//         </div>
-//         <div className="hidden md:flex md:items-center md:ml-6 space-x-4">
-//           <a href="https://www.linkedin.com/in/markfeher8/" target="_blank" className="social-icon">
-//             <img src={null} alt="LinkedIn" className="h-6 w-6" />
-//           </a>
-//           <a href="https://github.com/kezdetphia" target="_blank" className="social-icon">
-//             <img src={null} alt="GitHub" className="h-6 w-6" />
-//           </a>
-//           <Link to="#connect">
-//             <button className="btn-cta">
-//               <span>Let's Connect</span>
-//             </button>
-//           </Link>
-//         </div>
-//       </div>
-//     </nav>
-//   );
-// };
-
-// export default NavBar
+export default Nav;
