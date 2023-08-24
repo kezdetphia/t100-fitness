c6c80d7404488bd178bb1a79be2b41aaf02f7a17
kezdetphia <fehermark88@gmail.com>
2023-08-24 12:07:42 +0200

    email form mobile friendly

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import p1 from "../utilities/p1.png";
 
 const Contact = () => {
   const formInitialDetails = {
@@ -22,18 +23,23 @@ const Contact = () => {
   };
 
   return (
-   <div className="flex h-screen justify-start items-center  bg-gray-800">
-  <div className="w-2/3 px-10 py-10  flex flex-col items-center bg-transparent ">
-    <h1 className="text-white text-3xl pb-20 justify-start font-[Poppins]">Get in touch</h1>
-    <form onSubmit={handleSubmit} className="flex flex-col space-y-12">
-      <div className="flex space-x-4 w-full">
+    <div className="min-h-screen bg-gray-800">
+  <div className="p-4 text-center">
+    <h1 className="text-white text-3xl pb-4 font-[Poppins]">Get in touch</h1>
+  </div>
+  <div className="flex flex-col lg:flex-row p-4 space-y-4 lg:space-y-0">
+    <div className="lg:w-1/3 p-4 text-center">
+      <img src={p1} alt="Your Image" className="mx-auto max-w-xs h-auto" />
+    </div>
+    <div className="w-full lg:w-2/3 px-4 lg:px-10 py-10 bg-transparent">
+      <form onSubmit={handleSubmit} className="flex flex-col space-y-6">
         <input
           type="text"
           name="firstname"
           value={formDetails.firstName}
           placeholder="First name"
           onChange={(e) => onFormUpdate("firstName", e.target.value)}
-          className="flex-1 bg-transparent border border-white rounded-xl p-3 text-xl text-white font-[Poppins] "
+          className="bg-transparent border border-white rounded-lg p-2 text-white font-[Poppins] w-full"
         />
         <input
           type="text"
@@ -41,17 +47,15 @@ const Contact = () => {
           value={formDetails.lastName}
           onChange={(e) => onFormUpdate("lastName", e.target.value)}
           placeholder="Last name"
-          className="flex-1 bg-transparent border border-white rounded-xl p-3 text-xl text-white font-[Poppins] "
+          className="bg-transparent border border-white rounded-lg p-2 text-white font-[Poppins] w-full"
         />
-      </div>
-      <div className="flex space-x-4 w-full">
         <input
           type="email"
           name="email"
           value={formDetails.email}
           onChange={(e) => onFormUpdate("email", e.target.value)}
           placeholder="Email address"
-          className="flex-1 bg-transparent border border-white rounded-xl p-3 text-xl text-white font-[Poppins] "
+          className="bg-transparent border border-white rounded-lg p-2 text-white font-[Poppins] w-full"
         />
         <input
           type="tel"
@@ -59,25 +63,28 @@ const Contact = () => {
           value={formDetails.phone}
           onChange={(e) => onFormUpdate("phone", e.target.value)}
           placeholder="Phone Number"
-          className="flex-1 bg-transparent border border-white rounded-xl p-3 text-xl text-white font-[Poppins] "
+          className="bg-transparent border border-white rounded-lg p-2 text-white font-[Poppins] w-full"
         />
-      </div>
-      <textarea
-        name="message"
-        value={formDetails.message}
-        onChange={(e) => onFormUpdate("message", e.target.value)}
-        placeholder="Message"
-        className="w-full bg-transparent border border-white rounded-xl p-3 text-xl text-white font-[Poppins] "
-      />
-      <button
-        type="submit"
-        className="bg-blue-500 text-white py-2 px-4 rounded"
-      >
-        <span>{buttonText}</span>
-      </button>
-    </form>
+        <textarea
+          name="message"
+          value={formDetails.message}
+          onChange={(e) => onFormUpdate("message", e.target.value)}
+          placeholder="Message"
+          rows="4"
+          className="bg-transparent border border-white rounded-lg p-2 text-white font-[Poppins] w-full"
+        />
+        <button
+          type="submit"
+          className="bg-blue-500 text-white py-2 px-4 rounded w-full"
+        >
+          <span>{buttonText}</span>
+        </button>
+      </form>
+    </div>
   </div>
 </div>
+
+  
   );
 };
 
