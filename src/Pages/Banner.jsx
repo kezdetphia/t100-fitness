f2f19676ed5671a022b705e7f94453f13f669fdf
kezdetphia <fehermark88@gmail.com>
2023-10-05 23:31:42 -0700

    banner contactus button scrolls to contactus form

--- src/Pages/Banner.jsx
+++ src/Pages/Banner.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import bannerbg2 from "../utilities/bannerbg2.jpg";
 import Button from '../components/Button'
 import HeaderText from "../components/HeaderText";
+import { Link } from "react-scroll";
 
 const Banner2 = ({text, text1, text2}) => {
   const [isBannerLoaded, setIsBannerLoaded] = useState(false);
@@ -14,7 +15,7 @@ const Banner2 = ({text, text1, text2}) => {
 
   return (
     <div
-    id='banner'
+      id="banner"
       className="w-screen h-screen relative flex flex-col md:flex-row sm:flex-row pt-10"
       style={{
         backgroundImage: `linear-gradient(rgba(0, 0, 0, 1), rgba(0, 0, 0, 0.1)), url(${bannerbg2})`,
@@ -29,11 +30,21 @@ const Banner2 = ({text, text1, text2}) => {
         }`}
       >
         <div>
-          <HeaderText text1="UNLEASH YOUR" text2="POTENTIAL"/>
-         
-          <p className="font-[poppins, bold] text-sm text-customgray tracking-wider m-2">PROFESSIONAL ONLINE FITNESS COACHING</p>
-          <Button text=' CONTACT US' />
+          <HeaderText text1="UNLEASH YOUR" text2="POTENTIAL" />
 
+          <p className="font-[poppins, bold] text-sm text-customgray tracking-wider m-2">
+            PROFESSIONAL ONLINE FITNESS COACHING
+          </p>
+          <Link
+            to={"contactUs"}
+            spy={true}
+            smooth={true}
+            offset={-65}
+            duration={500}
+          >
+            {" "}
+            <Button text=" CONTACT US" />{" "}
+          </Link>
         </div>
       </p>
     </div>
