76c43169fbd2c72a0ac5f8848076a14fed8c8cd6
kezdetphia <fehermark88@gmail.com>
2023-10-09 18:00:40 -0700

    footer icon aling, oages and links align

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -9,19 +9,19 @@ const Footer = ({ text1, text2, customsize }) => {
   return (
     <div className="w-full h-full bg-customblackblack flex-row md:flex pt-5 ">
       {/* left-div */}
-      <div className="flex items-center w-1/3 ">
+      <div className="flex  sm:w-1/3  ">
         {/* headertext */}
-        <div>
+        <div >
           <HeaderText text1={"MAJER"} text2={"FITNESS"} />
           <div>
-            <p className="text-customgray pl-5 text-start  font-poppins text-sm leading-1 tracking-wide">
+            <p className="text-customgray sm:pl-5 flex text-center px-5  sm:text-start   font-poppins text-sm leading-1 tracking-wide">
               Lorem ipsum dolor sit amet consectetur. Nulla et nisl sed diam
               aliquam. Urna morbi egestas dis velit. Non viverra fermentum sit
               sit neque rutrum habitasse auctor facilisi fermentum sit et sed.
             </p>
           </div>
 
-          <div className="w-full flex items-center p-5 space-x-4 ">
+          <div className="icons w-full sm:justify-start flex justify-evenly   pt-14  p-5 space-x-4 ">
             <FacebookIcon customsize={"w-10 mr-3"} />
             <InstagramIcon customsize={"w-10 mr-3"} />
             <FacebookIcon customsize={"w-10 mr-3"} />
@@ -29,14 +29,14 @@ const Footer = ({ text1, text2, customsize }) => {
           </div>
         </div>
       </div>
-      
+
       {/* mid-div */}
-      <div className="p-5 sm:pl-20 text-left">
-        <h1 className="text-customwhite font-poppins font-bold text-lg sm:pb-3">
+      <div className="p-5 sm:pl-20 text-left space-y-2">
+        <h1 className="text-customwhite text-center sm:text-left font-poppins font-bold text-lg pb-3">
           Pages
         </h1>
 
-        <div className="text-customgray flex sm:flex-col space-x-3 sm:space-x-0  sm:space-y-1 cursor-pointer ">
+        <div className="text-customgray flex sm:flex-col space-x-3 sm:space-x-0 justify-evenly  sm:space-y-1 cursor-pointer ">
           <Link
             to={"banner"}
             spy={true}
